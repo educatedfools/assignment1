9cac2b5274418148082ba6f7b7b3cb52a49265b2
Mike R <Mike R>
2016-06-19 23:25:43 -0700

    assignment 1 upload

---
+++ assignment1/script.js
@@ -0,0 +1,167 @@
+//STEP 1
+//var intSomeMonth;
+//function theMonth();
+//var arrSummerMonth;
+//function myLibrary();
+
+//STEP 2
+//var intNumber = 1 + 1;
+//var strString = "this is a string expression";
+//var boolean = (10 > 9);
+//var expression = null;
+
+//STEP 3
+//var complexExpression = 1 + 1;
+//window.console.log(complexExpression);
+
+//STEP 4
+//var sFirstName, sLastName, sAddress, sCity, sState, nZipCode, nAge, sReferralSource, bMayWeContact;
+//
+////STEP 5
+//sFirstName = "Mike";
+//sLastName = "Rhodes";
+//sAddress = "123 home way";
+//sCity = "San Diego";
+//sState = "CA";
+//nZipCode = 90210;
+//nAge = 34;
+//var arrUserProfile = [sFirstName, sLastName, sAddress, sCity, sState, nZipCode, nAge];
+
+//STEP 6
+//var sStringNum = 3 + " this is a string";
+//window.console.log(sStringNum);
+//
+//var sBoolStr = (10 > 9) + " this is a string";
+//window.console.log(sBoolStr);
+//
+//var nNumBool = (10 > 9) + false;
+//window.console.log(nNumBool);
+//var x = false + false;
+//var y = true + true;
+//window.console.log(x);
+//window.console.log(y);
+//I don't understand the results here. var nNumBool was for the assignment, the other two for practice. The only thing I can think of is that true = 1 and false = 0 and because there's no string involved there is no type coersion and it becomes number addition. Am I off base on this line of thinking?
+
+//STEP 7
+//window.console.log(x);
+//var x = 1;
+
+//STEP 8
+//No, I would escape I'm "I\'m"
+
+//STEP 9
+//var x = null;
+//window.console.log(x);
+//var y;
+//window.console.log(y);
+
+//STEP 10
+//var x = "this is a string";
+//window.console.log(typeof x);
+
+//var x = 111;
+//window.console.log(typeof x);
+//
+//var x = true;
+//window.console.log(typeof x);
+//
+//var x = [1, 2, 3];
+//window.console.log(typeof x);
+
+//var x;
+//window.console.log(typeof x);
+
+//STEP 11
+//window.alert("Hello " + "Mike Rhodes" + ", welcome to the Javascript class!");
+
+//STEP 12
+//var sName = "Mike Rhodes";
+//window.alert("Hello " + sName + ", welcome to the Javascript class!");
+
+//STEP 13
+//var sName = "Mike Rhodes";
+//var sClassName = "Javascript";
+//window.alert("Hello " + sName + ", welcome to the " + sClassName + " class!");
+
+//STEP 14
+//var sName = "Mike Rhodes";
+//var sClassName = "Javascript";
+//window.alert("Hello " + sName + ".\nWelcome to the " + sClassName + " class!");
+
+//STEP 15
+//var sName = window.prompt("What is your name?");
+//var sClassName = "Javascript";
+//window.alert("Hello " + sName + ".\nWelcome to the " + sClassName + " class!");
+
+//STEP 16
+//var sName = window.prompt("What is your name?");
+//var sClassName = window.prompt("What class are you taking?");
+//window.alert("Hello " + sName + ".\nWelcome to the " + sClassName + " class!");
+
+//STEP 17
+//var x = 10;
+//var y = 20;
+//window.console.log(x + y);
+
+//STEP 18
+//var x = 20 + 20;
+//window.console.log(x);
+
+//STEP 19
+//var x = 20 * 5;
+//window.console.log(x);
+
+//STEP 20
+//var x = 20 % 3 / 1;
+//window.console.log(x);
+//if 20 % 3 = 2 then what is the point of dividing by 1? thinking I may have misunderstood the instruction here
+
+//STEP 21
+//var x = 1
+//if(x < 2 || x > 0 ) {
+//    x = true;
+//    window.console.log(x);
+//}
+//else {
+//    x = false;
+//    window.console.log(x);
+//};
+
+//STEP 22
+//var x = 10
+//if(x <= 10 && x >= 20) {
+//    x = true;
+//    window.console.log(x);
+//}
+//else {
+//    x = false;
+//    window.console.log(x);
+//};
+
+//STEP 23
+//var widget = {
+//    property1: "hello"
+//};
+//window.console.log(typeof widget);
+
+//STEP 24
+//var widget = {
+//    numbers: 4,
+//    color: "black"
+//};
+//window.console.log(widget instanceof Object);
+//OOP is weird and not easy to understand!
+
+//STEP 25
+//var widget = {
+//    numbers: 4,
+//    color: "black"
+//};
+//window.console.log(widget.numbers instanceof Object);
+
+//Wasn't expecting to encounter totally new concepts in the first week. Have a feeling this class is going to be a welcome challenge. Thanks Zak.
+
+
+
+
+
